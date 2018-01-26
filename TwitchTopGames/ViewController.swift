496fba789b8c68854b70ef66eeb717ee724cbf72
Mauro André Barros Mazzola <mauroandre.mazzola@hotmail.com>
2018-01-26 16:05:29 -0200

    Initial Commit

---
+++ TwitchTopGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitchTopGames
+//
+//  Created by Mauro André Barros Mazzola on 26/01/18.
+//  Copyright © 2018 Mauro André Barros Mazzola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
